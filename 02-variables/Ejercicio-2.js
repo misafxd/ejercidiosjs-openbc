3be251b8a27725b5236a28a0cb23d70a727b8c8e
misafxd <pisichu.mafs@gmail.com>
2023-06-16 16:59:37 -0600

    Update Ejercicio-2.js

--- 02-variables/Ejercicio-2.js
+++ 02-variables/Ejercicio-2.js
@@ -3,7 +3,7 @@ let ahora = new Date(2023,5,16);
 const libro ={
     "titulo": "Cronicas Marcianas",
     "autor": "Ray Bradbury",
-    "fecha": 1945,
+    "fecha": new Date(1945),
     "url": "https://www.amazon.com.mx/Cronicas-Marcianas-Ray-Bradbury/dp/8445076531"
 }
 
