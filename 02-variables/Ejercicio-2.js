e610a75fa3abdb27bd2cb23ab7c16c1f2117e3f9
misafxd <pisichu.mafs@gmail.com>
2023-06-16 16:58:13 -0600

    Add files via upload

---
+++ 02-variables/Ejercicio-2.js
@@ -0,0 +1,12 @@
+let ahora = new Date(2023,5,16);
+
+const libro ={
+    "titulo": "Cronicas Marcianas",
+    "autor": "Ray Bradbury",
+    "fecha": 1945,
+    "url": "https://www.amazon.com.mx/Cronicas-Marcianas-Ray-Bradbury/dp/8445076531"
+}
+
+const lista = ["Misael", 29, true, ahora.getFullYear(),libro];
+
+console.log(lista);
