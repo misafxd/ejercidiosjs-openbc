f3228df6be84913d1f60ab24035ff97cdf103b25
misafxd <pisichu.mafs@gmail.com>
2023-06-20 13:12:07 -0600

    Add files via upload

---
+++ 05-numeros/Numeros.js
@@ -0,0 +1,11 @@
+let miAlturaCentimetros = 170;
+let miAlturaMetros = 1.7;
+let miPesoKilogramos = 63.2;
+
+let miAlturaRedArriba = Math.ceil(miAlturaMetros);
+let miAlturaRedAbajo = Math.floor(miAlturaMetros)
+
+let valor = Number.MAX_VALUE +1 === Number.MAX_VALUE;
+
+
+
