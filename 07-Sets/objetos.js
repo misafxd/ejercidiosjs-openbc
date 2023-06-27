af3105db479329be8af3fb5f026fe113e6de1bb4
misafxd <pisichu.mafs@gmail.com>
2023-06-26 20:10:13 -0600

    Add files via upload

---
+++ 07-Sets/objetos.js
@@ -0,0 +1,19 @@
+//Ejercicio 2
+const obj = {
+    nombre: 'Misael',
+    apellido: 'Flores',
+    edad: 29,
+    altura: 170,
+    eresDesarrollador: true
+};
+
+let miEdad = obj.edad;
+
+
+const miLista = [
+    {...obj}, 
+    {nombre: 'Angel', apellido: 'Rosas', edad: 39, altura: 168, eresDesarrollador: true},
+    {nombre: 'erika', apellido: 'Martinez', edad: 30, altura: 155, eresDesarrollador: true}
+];
+
+const listaOrdenada = miLista.sort((a,b)=> a.edad -b.edad);
